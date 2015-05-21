5e14a5be93ec9917ed7b2bf7f63effb97626677b
James Gabrielsen <jgabrielsen@daz3d.com>
2015-05-21 11:44:18 -0600

    Adding options to profile menus and fixing styling

--- applications/dashboard/views/modules/profileoptions.php
+++ applications/dashboard/views/modules/profileoptions.php
@@ -1,14 +1,14 @@
 <?php if (!defined('APPLICATION')) exit();
 /** Displays the "Edit My Profile" or "Back to Profile" buttons on the top of the profile page. */
 ?>
-<div class="ProfileOptions button">
+<div class="ProfileOptions">
    <?php
    $Controller = Gdn::Controller();
    $Controller->FireEvent('BeforeProfileOptions');
-   echo ButtonGroup($Controller->EventArguments['MemberOptions'], 'NavButton MemberButtons');
+   echo ButtonGroup($Controller->EventArguments['MemberOptions'], 'NavButton MemberButtons button');
    echo ' ';
    echo ButtonDropDown($Controller->EventArguments['ProfileOptions'], 
-      'NavButton ProfileButtons Button-EditProfile',
+      'NavButton ProfileButtons Button-EditProfile button',
       Sprite('SpEditProfile', 'Sprite16').' <span class="Hidden">'.T('Edit Profile').'</span>'
    );
    ?>
