0100a0e71fbd18479fb4e865e00aed1f261e4f71
Benjam Welker <bwelker@daz3d.com>
2015-06-18 12:21:11 -0600

    make selector a little more specific

--- themes/daz3d/js/custom.js
+++ themes/daz3d/js/custom.js
@@ -2,6 +2,6 @@ jQuery(document).ready( function($) {
 	"use strict";
 
 	// open all external links in new window
-	$('a').not('a[href*="' + window.location.hostname +'"]').attr('target', '_blank');
+	$('a[href^="//"]').add('a[href^="http"]').not('a[href*="' + window.location.hostname +'"]').attr('target', '_blank');
 
 });
