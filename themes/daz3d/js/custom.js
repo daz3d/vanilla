0a0431ae4a8c603eebf7246d876baed184cbd532
Benjam Welker <benjamwelker@gmail.com>
2015-07-21 00:27:44 -0600

    fix scrolling when linked directly

--- themes/daz3d/js/custom.js
+++ themes/daz3d/js/custom.js
@@ -1,7 +1,13 @@
 jQuery(document).ready( function($) {
+
 	"use strict";
 
 	// open all external links in new window
 	$('a[href^="//"]').add('a[href^="http"]').not('a[href*="' + window.location.hostname +'"]').attr('target', '_blank');
+
+	// if user got here via direct post link, scroll up a bit to bring full post back into view
+	if (1 === document.location.hash.indexOf('Comment')) {
+		$('body').scrollTop($('body').scrollTop( ) + 120);
+	}
 
 });
