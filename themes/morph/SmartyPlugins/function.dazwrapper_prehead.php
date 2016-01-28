086757d7f8b73a457799e2ea61a618d5fa621d49
Benjam Welker <benjamwelker@gmail.com>
2016-01-27 23:49:20 -0700

    DRY the wrapper builder and send/collect cookies from magento
    possible help for looping bug

--- themes/morph/SmartyPlugins/function.dazwrapper_prehead.php
+++ themes/morph/SmartyPlugins/function.dazwrapper_prehead.php
@@ -1,17 +1,23 @@
 <?php if (!defined('APPLICATION')) exit();
 
+include(dirname(__FILE__).DIRECTORY_SEPARATOR.'bootstrap.php');
+
 function smarty_function_dazwrapper_prehead($Params, &$Smarty) {
 	if ( ! $Smarty->get_template_vars('DAZ_Wrapper')) {
-		$ch = curl_init(Gdn::Config('Daz.RootUrl') . Gdn::Config('Daz.WrapperUrl'));
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		$wrapperHtml = curl_exec($ch);
-		curl_close($ch);
+		get_daz_wrapper($Smarty);
+	}
 
-		$Smarty->assign('DAZ_Wrapper', $wrapperHtml);
+	// send along any cookies from the magento side
+	$header = explode("\n", str_replace(array("\r\n", "\n\r", "\r"), "\n", $Smarty->get_template_vars('DAZ_Header')));
+
+	foreach ($header as $row) {
+		if (false !== stripos($row, 'set-cookie:')) {
+			// send the cookie, but DO NOT replace any previously set cookie headers
+			header($row, false);
+		}
 	}
-	else {
-		$wrapperHtml = $Smarty->get_template_vars('DAZ_Wrapper');
-	}
+
+	$wrapperHtml = $Smarty->get_template_vars('DAZ_Wrapper');
 
 	// from the start to just before the open <title> tag
 	$header = substr($wrapperHtml, 0, strpos($wrapperHtml, '<title>'));
