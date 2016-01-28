ee5339bb272a2512953c0417e1d455241153e070
Benjam Welker <bwelker@daz3d.com>
2016-01-28 16:11:38 -0700

    fix minor errors

--- themes/morph/views/modules/userinfo.php
+++ themes/morph/views/modules/userinfo.php
@@ -2,47 +2,45 @@
 $Session = Gdn::Session();
 if (Gdn::Config('Garden.Profile.ShowAbout')) {
    require_once Gdn::Controller()->FetchViewLocation('helper_functions', 'Profile', 'Dashboard');
-   
+
 ?>
 <div class="About P">
    <h2 class="H self-clearing"><?php echo T('About'); ?></h2>
-   <dl class="About self-clearing">
+   <ul class="About self-clearing none">
       <?php
       if ($this->User->Banned) {
-         echo '<dd class="Value"><span class="Tag Tag-Banned">'.T('Banned').'</span></dd>';
+         echo '<li><dl><dd class="Value"><span class="Tag Tag-Banned">'.T('Banned').'</span></dd></dl></li>';
       }
       ?>
 
-      <div class="UserDetail">
+      <li><dl>
          <dt class="Name"><?php echo T('Username'); ?></dt>
-         <dd class="Name" itemprop="name"><?php echo $this->User->Name; ?></dd>
-      </div>
+         <dd class="Name"><?php echo $this->User->Name; ?></dd>
+      </dl></li>
 
-      <div class="UserDetail">      
-         <?php               
-         if ($this->User->Email && ($this->User->ShowEmail || $Session->CheckPermission('Garden.Moderation.Manage'))) {
-            echo '<dt class="Email">'.T('Email').'</dt>
-            <dd class="Email" itemprop="email">'.Gdn_Format::Email($this->User->Email).'</dd>';
-         }
-         ?>
-      </div>
+      <?php if ($this->User->Email && ($this->User->ShowEmail || $Session->CheckPermission('Garden.Moderation.Manage'))) : ?>
+      <li><dl>
+         <dt class="Email"><?php echo T('Email'); ?></dt>
+         <dd class="Email"><?php echo Gdn_Format::Email($this->User->Email); ?></dd>
+      </dl></li>
+      <?php endif; ?>
 
-      <div class="UserDetail">     
+      <li><dl>
          <dt class="Joined"><?php echo T('Joined'); ?></dt>
          <dd class="Joined"><?php echo Gdn_Format::Date($this->User->DateFirstVisit, 'html'); ?></dd>
-      </div>
+      </dl></li>
 
-      <div class="UserDetail">
+      <li><dl>
          <dt class="Visits"><?php echo T('Visits'); ?></dt>
          <dd class="Visits"><?php echo number_format($this->User->CountVisits); ?></dd>
-      </div>
-      
-      <div class="UserDetail">
+      </dl></li>
+
+      <li><dl>
          <dt class="LastActive"><?php echo T('Last Active'); ?></dt>
          <dd class="LastActive"><?php echo Gdn_Format::Date($this->User->DateLastActive, 'html'); ?></dd>
-      </div>
+      </dl></li>
 
-      <div class="UserDetail">
+      <li><dl>
          <dt class="Roles"><?php echo T('Roles'); ?></dt>
          <dd class="Roles"><?php
             if (Gdn::Session()->CheckPermission('Garden.Moderation.Manage')) {
@@ -61,7 +59,7 @@ if (Gdn::Config('Garden.Profile.ShowAbout')) {
 
                // strip off the underscore prefix
                if (($this->User->UserID == Gdn::Session()->UserID) && ! CheckPermission('Garden.Moderation.Manage')) {
-                  $Role['Name'] = preg_replace('%^_%', '', $Role['Name']);
+                  $Role = preg_replace('%^_%', '', $Role);
                }
             }
 
@@ -73,38 +71,38 @@ if (Gdn::Config('Garden.Profile.ShowAbout')) {
             }
 
          ?></dd>
-      </div>
+      </dl></li>
 
       <?php if ($Points = GetValueR('User.Points', $this, 0)) : // Only show positive point totals  ?>
-      <div class="UserDetail">
+      <li><dl>
          <dt class="Points"><?php echo T('Points'); ?></dt>
          <dd class="Points"><?php echo number_format($Points); ?></dd>
-      </div>
+      </dl></li>
       <?php endif; ?>
 
       <?php if ($Session->CheckPermission('Garden.Moderation.Manage')) : ?>
-      <div class="UserDetail">
+      <li><dl>
          <dt class="IP"><?php echo T('Register IP'); ?></dt>
-         <dd class="IP"><?php 
+         <dd class="IP"><?php
             $IP = IPAnchor($this->User->InsertIPAddress);
             echo $IP ? $IP : T('n/a');
          ?></dd>
-      </div>
+      </dl></li>
 
-      <div class="UserDetail">
+      <li><dl>
          <dt class="IP"><?php echo T('Last IP'); ?></dt>
          <dd class="IP"><?php
             $IP = IPAnchor($this->User->LastIPAddress);
             echo $IP ? $IP : T('n/a');
          ?></dd>
-      </div>
+      </dl></li>
       <?php endif; ?>
 
-      <div class="UserDetail">
+      <li><dl>
          <?php $this->FireEvent('OnBasicInfo'); ?>
-      </div>
+      </dl></li>
 
-   </dl>
+   </ul>
 </div>
 <?php
 }
